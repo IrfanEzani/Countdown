58b2c03fc24df59ef3eae4f96368e8393dabe795
Irfan Ezani <irfanezani@gmail.com>
2020-05-02 00:13:23 +0800

    basic app functionality complete

---
+++ app.js
@@ -0,0 +1,27 @@
+var countDownDate = new Date("April 23, 2020 14:50:00").getTime();
+
+
+var countdown = setInterval(function() {
+
+var currentDate = new Date().getTime();
+let timeremain = Math.floor((countDownDate - currentDate)/1000);
+
+var days = Math.floor(timeremain / (60 * 60 * 24));
+var hours = Math.floor((timeremain % ( 60 * 60 * 24)) / (60 * 60));
+var minutes = Math.floor((timeremain % (60 * 60)) / (60));
+var seconds = Math.floor((timeremain % (60)));
+
+
+document.getElementById('dy').innerHTML = days
+document.getElementById('hr').innerHTML = hours
+document.getElementById('mn').innerHTML = minutes
+document.getElementById('sc').innerHTML = seconds
+
+if (timeremain < 0) {
+  document.getElementById('dy').innerHTML = "D"
+  document.getElementById('hr').innerHTML = "O"
+  document.getElementById('mn').innerHTML = "N"
+  document.getElementById('sc').innerHTML = "E"
+}
+
+},1000);
